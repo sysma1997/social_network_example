4e930969c9099f36ed9164c3e04958f95d9d3c78
Sebastian Moreno Acero <sysma1997@gmail.com>
2021-08-07 10:52:26 -0500

    Finish responsive in navbar frontend

--- frontend/src/components/panel/navbar/Navbar.tsx
+++ frontend/src/components/panel/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { User } from '../../../user/domain/User'
 import styles from './Navbar.module.css'
@@ -13,9 +14,16 @@ export const Navbar = (props: NavbarProps) => {
     const { user } = props
     const router = useRouter()
 
+    const [showMenuResponsive, setShowMenuResponsive] = useState<boolean>(false)
+
     const signOut = () => {
         localStorage.removeItem("token")
         router.push("/")
+    }
+
+    const onClickShowMenuResponsive = () => {
+        setShowMenuResponsive(!showMenuResponsive)
+        console.log(showMenuResponsive)
     }
 
     return <nav className={styles.navbar}>
@@ -23,9 +31,11 @@ export const Navbar = (props: NavbarProps) => {
             <label className={styles.navSysma}>SYSMA</label>
         </div>
         <div>
-            <Button className={styles.navbarMenuResponsive}>⁝</Button>
+            <Button className={styles.navbarMenuResponsive}
+                onClick={onClickShowMenuResponsive}>⁝</Button>
         </div>
-        <div className={`${styles.navColumn} ${styles.navMedium}`}>
+        <div className={`${styles.navColumn} ${styles.navMedium} ` +
+            `${(showMenuResponsive) && styles.showMenuResponsive}`}>
             <div className={styles.navSearchContent}>
                 <Input className={styles.navSearch} placeholder="Search..." />
                 <Button className={styles.navSearchButton}>Search</Button>
